48178684039a345f0c1130391f0dcdb0237a28c2
little-yus <159779618+little-yus@users.noreply.github.com>
2024-03-07 19:34:40 +0200

    Section 3.5 solutions

--- Math2001/03_Parity_and_Divisibility/05_Bezout.lean
+++ Math2001/03_Parity_and_Divisibility/05_Bezout.lean
@@ -14,10 +14,20 @@ example {n : ℤ} (hn : 8 ∣ 5 * n) : 8 ∣ n := by
 
 
 example {n : ℤ} (hn : 8 ∣ 5 * n) : 8 ∣ n := by
-  sorry
+  obtain ⟨a, ha⟩ := hn
+  use (5 * a - 3 * n)
+  calc
+    n = 5 * (5 * n) - 24 * n := by ring
+    _ = 5 * (8 * a) - 24 * n := by rw [ha]
+    _ = 8 * (5 * a - 3 * n) := by ring
 
 example {n : ℤ} (h1 : 5 ∣ 3 * n) : 5 ∣ n := by
-  sorry
+  obtain ⟨x, hx⟩ := h1
+  use 2 * x - n
+  calc
+    n = 2 * (3 * n) - 5 * n := by ring
+    _ = 2 * (5 * x) - 5 * n := by rw [hx]
+    _ = 5 * (2 * x - n) := by ring
 
 example {m : ℤ} (h1 : 8 ∣ m) (h2 : 5 ∣ m) : 40 ∣ m := by
   obtain ⟨a, ha⟩ := h1
@@ -33,13 +43,40 @@ example {m : ℤ} (h1 : 8 ∣ m) (h2 : 5 ∣ m) : 40 ∣ m := by
 
 
 example {n : ℤ} (hn : 6 ∣ 11 * n) : 6 ∣ n := by
-  sorry
+  obtain ⟨x, hx⟩ := hn
+  use (2 * n - x)
+  calc
+    n = 12 * n - 11 * n := by ring
+    _ = 12 * n - (6 * x) := by rw [hx]
+    _ = 6 * (2 * n - x) := by ring
 
 example {a : ℤ} (ha : 7 ∣ 5 * a) : 7 ∣ a := by
-  sorry
+  obtain ⟨x, hx⟩ := ha
+  use (3 * x - 2 * a)
+  calc
+    a = 15 * a - 14 * a := by ring
+    _ = 3 * (5 * a) - 14 * a := by ring
+    _ = 3 * (7 * x) - 14 * a := by rw [hx]
+    _ = 7 * (3 * x - 2 * a) := by ring
 
 example {n : ℤ} (h1 : 7 ∣ n) (h2 : 9 ∣ n) : 63 ∣ n := by
-  sorry
+  obtain ⟨a, ha⟩ := h1
+  obtain ⟨b, hb⟩ := h2
+  use (4 * a - 5 * b)
+  calc
+    n = 36 * n - 35 * n := by ring
+    _ = 4 * 9 * n - 5 * 7 * n := by ring
+    _ = 4 * 9 * (7 * a) - 5 * 7 * n := by rw [ha]
+    _ = 4 * 9 * (7 * a) - 5 * 7 * (9 * b) := by rw [hb]
+    _ = 63 * (4 * a - 5 * b) := by ring
 
 example {n : ℤ} (h1 : 5 ∣ n) (h2 : 13 ∣ n) : 65 ∣ n := by
-  sorry
+  obtain ⟨a, ha⟩ := h1
+  obtain ⟨b, hb⟩ := h2
+  use (2 * a - 5 * b)
+  calc
+    n = 26 * n - 25 * n := by ring
+    _ = 2 * 13 * n - 5 * 5 * n := by ring
+    _ = 2 * 13 * (5 * a) - 5 * 5 * n := by rw [ha]
+    _ = 2 * 13 * (5 * a) - 5 * 5 * (13 * b) := by rw [hb]
+    _ = 65 * (2 * a - 5 * b) := by ring
