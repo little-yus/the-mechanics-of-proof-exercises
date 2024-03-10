3b17640f1512cb1530fc5a46034a27483fda395a
little-yus <159779618+little-yus@users.noreply.github.com>
2024-03-10 19:44:11 +0200

    Section 4.3 solutions

--- Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
+++ Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
@@ -21,7 +21,34 @@ example : ∃! a : ℝ, 3 * a + 1 = 7 := by
 
 
 example : ∃! x : ℚ, ∀ a, a ≥ 1 → a ≤ 3 → (a - x) ^ 2 ≤ 1 := by
-  sorry
+  use 2
+  dsimp
+  constructor
+  · intro a h1 h2
+    have h1 : a - 2 ≥ -1 := by addarith [h1]
+    have h2 : a - 2 ≤ 1 := by addarith [h2]
+    have h := sq_le_sq' h1 h2
+    calc
+      (a - 2) ^ 2 ≤ 1 ^ 2 := by rel [h]
+      _ = 1 := by numbers
+  · intro y hy
+    have h1 : (1 - y) ^ 2 ≤ 1
+    apply hy
+    · numbers
+    · numbers
+    have h2 : (3 - y) ^ 2 ≤ 1
+    apply hy
+    · numbers
+    · numbers
+    have h3 :=
+      calc
+        (y - 2) ^ 2 = ((1 - y) ^ 2 + (3 - y) ^ 2 - 2) / 2 := by ring
+        _ ≤ (1 + 1 - 2) / 2 := by rel [h1, h2]
+        _ = 0 := by numbers
+    have h4 : (y - 2) ^ 2 ≥ 0 := by extra
+    have h5 : (y - 2) ^ 2 = 0 := by apply le_antisymm h3 h4
+    cancel 2 at h5
+    addarith [h5]
 
 example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 = x) : x = 0 := by
   obtain ⟨a, ha1, ha2⟩ := hx
@@ -70,10 +97,51 @@ example : ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] := by
 
 
 example : ∃! x : ℚ, 4 * x - 3 = 9 := by
-  sorry
+  use 3
+  dsimp
+  constructor
+  · numbers
+  · intro y hy
+    calc
+      y = (4 * y - 3 + 3) / 4 := by ring
+      _ = (9 + 3) / 4 := by rw [hy]
+      _ = 3 := by numbers
 
 example : ∃! n : ℕ, ∀ a, n ≤ a := by
-  sorry
+  use 0
+  dsimp
+  constructor
+  · intro a
+    extra
+  · intro y hy
+    have h1 : y ≤ 0 := by apply hy
+    interval_cases y
+    numbers
 
 example : ∃! r : ℤ, 0 ≤ r ∧ r < 3 ∧ 11 ≡ r [ZMOD 3] := by
-  sorry
+  use 2
+  dsimp
+  constructor
+  · constructor
+    · numbers
+    · constructor
+      · numbers
+      · use 3
+        numbers
+  · intro y hy
+    obtain ⟨h1, h2, k, hk⟩ := hy
+    have hk1 := by
+      calc
+        3 * k = 11 - y := by rw [hk]
+        _ > 11 - 3 := by rel [h2]
+        _ = 8 := by numbers
+        _ > 3 * 2 := by numbers
+    cancel 3 at hk1
+    have hk2 := by
+      calc
+        3 * k = 11 - y := by rw [hk]
+        _ ≤ 11 - 0 := by rel [h1]
+        _ < 3 * 4 := by numbers
+    cancel 3 at hk2
+    interval_cases k
+    addarith [hk]
