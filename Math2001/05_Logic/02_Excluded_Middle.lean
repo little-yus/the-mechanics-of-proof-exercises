12ed56452e643022676c9882c6a9acb3d5de1c14
little-yus <159779618+little-yus@users.noreply.github.com>
2024-03-17 22:00:25 +0200

    Section 5.2 solutions

--- Math2001/05_Logic/02_Excluded_Middle.lean
+++ Math2001/05_Logic/02_Excluded_Middle.lean
@@ -79,11 +79,57 @@ example {P : Prop} (hP : ¬¬P) : P := by
 
 def Tribalanced (x : ℝ) : Prop := ∀ n : ℕ, (1 + x / n) ^ n < 3
 
+#eval ((1 : ℚ) + 2/3) ^ 3 < 3 -- 2 is not tribalanced
+
+theorem tribalanced_zero : Tribalanced 0 := by
+  intro n
+  conv => ring
+  numbers
+
+theorem not_tribalanced_two : ¬ Tribalanced 2 := by
+  intro h
+  dsimp [Tribalanced] at h
+  have h1 := h 1
+  numbers at h1
+
 example : ∃ x : ℝ, Tribalanced x ∧ ¬ Tribalanced (x + 1) := by
-  sorry
+  by_cases h : Tribalanced 1
+  · use 1
+    constructor
+    · apply h
+    · conv => numbers
+      apply not_tribalanced_two
+  · use 0
+    constructor
+    · apply tribalanced_zero
+    · conv => numbers
+      apply h
 
 example (P Q : Prop) : (¬P → ¬Q) ↔ (Q → P) := by
-  sorry
+  constructor
+  · intro hpq hq
+    by_cases hp : P
+    · apply hp
+    · have hnq := hpq hp
+      contradiction
+  · intro hqp hnp
+    by_cases hq : Q
+    · have hp := hqp hq
+      contradiction
+    · apply hq
 
 example : ∃ k : ℕ, Superpowered k ∧ ¬ Superpowered (k + 1) := by
-  sorry
+  use 1
+  constructor
+  · apply superpowered_one
+  · conv => numbers
+    intro h
+    dsimp [Superpowered] at h
+    have hprime := h 5
+    conv at hprime => numbers
+    have hnotprime : ¬ Prime 4294967297
+    · apply not_prime 641 6700417
+      · numbers
+      · numbers
+      · numbers
+    contradiction -- hprime and hnotprime
